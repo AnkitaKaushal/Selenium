ea1f4cb5bfdd57723366352630039582648adcc2
AnkitaKausha_jy2pd0o <ankita.kaushal@infostride.com>
2022-01-13 19:56:02 +0530

    Selenium Day1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,19 @@
 package com.company;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("Hello");
+//        System.setProperty("webdriver.chrome.driver", "C:\\Users\\AnkitaKausha_jy2pd0o\\Downloads\\chromedriver.exe");
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://google.com");
+        driver.close();
 
     }
 }
